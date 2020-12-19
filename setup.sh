868541f6736cb1374f8b3c0087f76037eba44724
Huey <hello@huey.xyz>
2020-12-19 13:11:11 +0800

    .vim folder

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # assume git clone to ~
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.ssh ~/.ssh
+ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.profile ~/.profile
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
