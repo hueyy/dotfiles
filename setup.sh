a1b30e3c1993f2274356137249a68f3c2165029e
Huey <hello@huey.xyz>
2020-12-19 12:55:21 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# assume git clone to ~
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.profile ~/.profile
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
