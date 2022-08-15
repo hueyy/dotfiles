73b11d64fb44ade6509d0bb5c1842936d1907ac3
Huey <hello@huey.xyz>
2022-08-15 21:21:38 +0800

    add new tools and update versions

--- .zshrc
+++ .zshrc
@@ -112,3 +112,7 @@ source $HOME/.profile
 # Generated for envman. Do not edit.
 [ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 # assume git clone to ~
-sudo sh -c "echo 'deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' >> /etc/apt/sources.list"
+sudo add-apt-repository -yn deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main
 wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
-echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+sudo add-apt-repository -yn deb https://deb.volian.org/volian/ scar main
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-sudo apt update && sudo apt install git vim nano ncdu zsh git-secret exa nala -y
+sudo apt update && sudo apt install -y nala && sudo nala install -y git vim nano ncdu zsh git-secret nala bat
 mkdir -p ~/.bin
 
 ln -s ~/.dotfiles/.ssh ~
@@ -21,38 +21,38 @@ chsh -s $(which zsh)
 DOWNLOADS_FOLDER=~/Downloads
 
 # lsd
-LSD_FILENAME=lsd-musl_0.20.1_amd64.deb
-wget -P $DOWNLOADS_FOLDER "https://github.com/Peltoche/lsd/releases/download/0.20.1/$LSD_FILENAME"
-sudo apt install -y $DOWNLOADS_FOLDER/$LSD_FILENAME
+LSD_FILENAME=lsd-musl_0.22.0_amd64.deb
+wget -P $DOWNLOADS_FOLDER "https://github.com/Peltoche/lsd/releases/download/0.22.0/$LSD_FILENAME"
+sudo nala install -y $DOWNLOADS_FOLDER/$LSD_FILENAME
 rm $DOWNLOADS_FOLDER/$LSD_FILENAME
 
 # delta
-DELTA_FILENAME=git-delta_0.8.2_amd64.deb
-wget -P $DOWNLOADS_FOLDER "https://github.com/dandavison/delta/releases/download/0.8.2/$DELTA_FILENAME"
-sudo apt install -y $DOWNLOADS_FOLDER/$DELTA_FILENAME
+DELTA_FILENAME=git-delta_0.13.0_amd64.deb
+wget -P $DOWNLOADS_FOLDER "https://github.com/dandavison/delta/releases/download/0.13.0/$DELTA_FILENAME"
+sudo nala install -y $DOWNLOADS_FOLDER/$DELTA_FILENAME
 rm $DOWNLOADS_FOLDER/$DELTA_FILENAME
 
 # duf
-DUF_FILENAME=duf_0.6.2_linux_amd64.deb
-wget -P $DOWNLOADS_FOLDER "https://github.com/muesli/duf/releases/download/v0.6.2/$DUF_FILENAME"
-sudo apt install -y $DOWNLOADS_FOLDER/$DUF_FILENAME
+DUF_FILENAME=duf_0.8.1_linux_amd64.deb
+wget -P $DOWNLOADS_FOLDER "https://github.com/muesli/duf/releases/download/v0.8.1/$DUF_FILENAME"
+sudo nala install -y $DOWNLOADS_FOLDER/$DUF_FILENAME
 rm $DOWNLOADS_FOLDER/$DUF_FILENAME
 
 # curlie
-CURLIE_FILENAME=curlie_1.6.0_linux_amd64.deb
-wget -P $DOWNLOADS_FOLDER "https://github.com/rs/curlie/releases/download/v1.6.0/$CURLIE_FILENAME"
-sudo apt install -y $DOWNLOADS_FOLDER/$CURLIE_FILENAME
+CURLIE_FILENAME=curlie_1.6.9_linux_amd64.deb
+wget -P $DOWNLOADS_FOLDER "https://github.com/rs/curlie/releases/download/v1.6.9/$CURLIE_FILENAME"
+sudo nala install -y $DOWNLOADS_FOLDER/$CURLIE_FILENAME
 rm $DOWNLOADS_FOLDER/$CURLIE_FILENAME
 
 # install node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install --lts=fermium
+nvm install --lts=gallium
 
 # zoxide
 curl -sS https://webinstall.dev/zoxide | bash
 
-npm i -g tldr gtop commitizen
+npm i -g gtop tldr
 
 source ~/.profile
